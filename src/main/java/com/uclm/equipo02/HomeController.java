ef838f65f5cd991c3ec142b65c39a1feae2a07fb
Rodrigo <rodrigodhv@gmail.com>
2018-10-30 19:59:48 +0100

    prueba mongo

--- src/main/java/com/uclm/equipo02/HomeController.java
+++ src/main/java/com/uclm/equipo02/HomeController.java
@@ -5,24 +5,15 @@ import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
-
-import org.bson.BsonDocument;
-import org.bson.BsonString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import com.uclm.equipo02.modelo.Usuario;
-import com.uclm.equipo02.persistencia.MongoBroker;
-import com.uclm.equipo02.persistencia.UsuarioDaoImplement;
+import com.uclm.equipo02.persistencia.UsuarioDao;
+
 
 
 @Controller
@@ -35,10 +26,6 @@ public class HomeController {
 
 	private final String usuarioServ = "usuario/";
 	private final String usuario_login = "usuario/login";
-	private MongoClientURI uri;
-	private MongoClient mongoClient;
-	private MongoDatabase db;
-	
 	private final String name = "nombre";
 	private final String password = "pwd";
 	private final String email = "email";
@@ -73,8 +60,7 @@ public class HomeController {
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public void iniciarSesion(HttpServletRequest request, Model model) throws Exception {
 		String cadenaUrl = usuarioServ;
-		System.out.println("He pasado por el método del iniciarSesion");
-		UsuarioDaoImplement userDao=  new UsuarioDaoImplement();
+		UsuarioDao userDao=  new UsuarioDaoImplement();
 		Usuario user = new Usuario();
 		user.setNombre("Rodrigo");
 		user.setPassword("1234");
@@ -84,8 +70,7 @@ public class HomeController {
 		try {
 			userDao.insert(user);
 		} catch (Exception e) {
-			System.out.println("estoy dentro de la excepción");
-			System.out.println(e.toString());
+			
 		}
 		//return cadenaUrl += "login";
 	}
